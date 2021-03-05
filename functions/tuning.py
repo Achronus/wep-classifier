085bc65c8dfa02fafed74dd5020b58564a3514ec
Ryan Partridge <rpartridge101@yahoo.co.uk>
2021-03-05 10:02:53 +0000

    Added functionality
    Four new functions: get_model_names, set_model, save_best_models, load_best_models

--- functions/tuning.py
+++ functions/tuning.py
@@ -41,7 +41,7 @@ class Tuner:
         self.labels = np.array(list(dataset.class_to_idx), dtype=object)
         return dataset
     
-    def _set_models(self, n_classes, h_layers):
+    def _set_initial_models(self, n_classes, h_layers):
         """
         Helper function used to set the three models (GoogLeNet, MobileNet v2, and ResNet-34) with new classifiers.
         
@@ -85,7 +85,7 @@ class Tuner:
             n_classes (int) - number of output classes
             h_layers (list) - hidden node integers, one per layer
         """
-        models = self._set_models(n_classes, h_layers)
+        models = self._set_initial_models(n_classes, h_layers)
         
         # Total params for each model
         for idx, model in enumerate(models):
@@ -132,7 +132,7 @@ class Tuner:
         # Iterate over hidden layers
         for l in range(len(h_layers)):
             # Create instances of pretrained CNN architectures
-            models = self._set_models(n_classes, h_layers[l])
+            models = self._set_initial_models(n_classes, h_layers[l])
 
             # Iterate over models
             for m in range(len(models)):
@@ -159,3 +159,137 @@ class Tuner:
                     # Train model
                     self.utils.train(models[m], train_loader, valid_loader, criterion, 
                                      optimizer, filepath, epochs, iterations, patience)
+
+    def get_model_names(self, model_paths):
+        """
+        Used to obtain the model names from a list of saved models filenames.
+        
+        Parameters:
+            model_paths (list) - saved model names as strings
+        """
+        model_names = []
+        
+        # Set model names
+        for item in model_paths:
+            model_names.extend(item.split('_')) # Split into individual components
+        
+        model_names = list(set(model_names)) # Create a unique list
+        model_names.sort() # Sort into order (numbers first)
+        model_names = model_names[-3:] # Get only model names
+        return model_names
+                    
+    def set_model(self, model_paths, model, model_name, batch_size, h_layers):
+        """
+        Used to check what type of model needs to be set for testing. Returns the model and its name.
+        
+        Name format: [model]_[batch_size]_[hidden_size]_[hidden_size]
+        
+        Parameters:
+            model_paths (list) - list of filepaths of saved models
+            model (torchvision.models) - initial pretrained model
+            model_name (string) - name of the model
+            batch_size (int) - number of dataset batches
+            hidden_size (list) - hidden node sizes as integers
+        """
+        # Set initial variables
+        load_name = ""
+        compare_parts = [model_name, batch_size]
+        compare_parts.extend(h_layers)
+        
+        # Iterate over each model
+        for filepath in model_paths:
+            compare_name = filepath.split('/')[-1].rstrip('.pt').split('_')
+
+            if compare_name[0] == compare_parts[0] \
+            and compare_name[1] == str(compare_parts[1]) \
+            and compare_name[-2] == str(compare_parts[-2]) \
+            and compare_name[-1] == str(compare_parts[-1]):
+                # Return saved model
+                load_name = filepath.split('/')[-1].rstrip('.pt')
+                self.utils.load_model(model, 'saved_models/' + filepath)
+                break
+        
+        return model, load_name
+    
+    def save_best_models(self, model_stats, model_names, n_classes):
+        """
+        Used to save the three best performing models based on the statistics of all model variations.
+        
+        Parameters:
+            model_stats (pandas.DataFrame) - table of best model statistics
+            model_names (list) - name of the models as strings
+            n_classes (int) - number of output classes
+        """
+        best_models = []
+        
+        # Iterate over each model
+        for idx, item in enumerate(model_stats['Name']):
+            name, batch, h1, h2 = item.split('_')
+            h_layers = [int(h1), int(h2)]
+            filepath = f'saved_models/{item}.pt'
+            cnn_models = self._set_initial_models(n_classes, h_layers)
+            
+            # Check names match
+            for cnn_name in model_names:
+                if name == cnn_name:
+                    # Load model and store it
+                    model = cnn_models[idx]
+                    self.utils.load_model(model, filepath)
+                    best_models.append(model)
+                    new_name = cnn_name.replace('-', '').lower()
+                    
+                    # Set statistics
+                    stats = list(model_stats.iloc[idx, 1:])
+                    stats = {'accuracy': stats[0], 'top-1 error': stats[1],
+                             'top-5 error': stats[2], 'precision': stats[3],
+                             'recall': stats[4], 'f1-score': stats[5]}
+                    
+                    # Set additional model parameters
+                    model.batch_size = batch
+                    model.h_layers = h_layers
+                    model.stats = stats
+                    
+                    # Save as best model
+                    self._save_model(model)
+        
+        return best_models
+    
+    def _save_model(self, model):
+        """
+        Helper function used to save the best models.
+        
+        Parameters:
+            model (torchvision.models) - model to save
+        """
+        torch.save({'parameters': model.state_dict(),
+                    'train_losses': model.train_losses,
+                    'valid_losses': model.valid_losses,
+                    'batch_size': model.batch_size,
+                    'h_layers': model.h_layers,
+                    'stats': model.stats, 
+                    }, f'saved_models/best_{new_name}.pt')
+        
+    def load_best_models(self, models, filenames):
+        """
+        Used to load the three best models.
+        
+        Parameters:
+            model (list) - torchvision.models to load
+            filenames (list) - filenames of saved models to load within saved_models folder
+        """
+        # Set a checkpoint
+        for idx, model in enumerate(models):
+            checkpoint = torch.load(f"saved_models/{filenames[i]}.pt")
+
+            # Store utility variables
+            model.train_losses = checkpoint['train_losses']
+            model.valid_losses = checkpoint['valid_losses']
+            model.batch_size = checkpoint['batch_size']
+            model.h_layers = checkpoint['h_layers']
+            model.stats = checkpoint['stats']
+
+            # load model parameters
+            model.load_state_dict(checkpoint['parameters'])
+        
+        print("Models loaded. Utility variables available:")
+        print("\ttrain_losses, valid_losses, batch_size, h_layers, stats, parameters.")
